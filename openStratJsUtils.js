5b5d4c45eb27b53cf7b7bc12f68c0b8574fdc3c1
w0d <35996314+w0d@users.noreply.github.com>
2020-04-25 13:19:04 +0100

    Update openStratJsUtils.js

--- openStratJsUtils.js
+++ openStratJsUtils.js
@@ -1,3 +1,9 @@
+//**BOOKMARKLETS**//
+//*mouse position:
+//javascript:(function()%7Bdocument.body.onmousemove%20%3D%20function(e)%7Bif%20(document.getElementById(%22mouseFeedback%22)%20%3D%3D%20null)%20%7Blet%20myDiv%20%3D%20document.createElement(%22div%22)%3BmyDiv.id%20%3D%20%22mouseFeedback%22%3BmyDiv.style%20%3D%20%22position%3Aabsolute%3Bleft%3A0px%3Btop%3A0px%3Bwidth%3A100px%3Bheight%3A14px%22%3Bdocument.body.appendChild(myDiv)%3B%7Dlet%20feedbackDiv%20%3D%20document.getElementById(%22mouseFeedback%22)%3Blet%20theCanvas%20%3D%20document.getElementById(%22scanv%22)%3BfeedbackDiv.innerText%20%3D%20%22%22%2B(e.clientX%20-%20theCanvas.width%2F2)%2B%22%2C%20%22%2B(-e.clientY%20%2B%20theCanvas.height%2F2)%3B%7D%7D)()
+//*drawOpenStratGrid:
+//javascript:(function()%7Bvar%20x%20%3D%20document.getElementById(%22scanv%22)%3Bvar%20context%20%3D%20x.getContext(%222d%22)%3Bvar%20width%20%3D%20context.canvas.width%3Bvar%20height%20%3D%20context.canvas.height%3Bvar%20centreX%20%3D%20width%2F2.0%3Bvar%20centreY%20%3D%20height%2F2.0%3Bcontext.beginPath()%3Bcontext.strokeStyle%20%3D%20'aqua'%3Bcontext.lineWidth%20%3D%200.25%3Bfor%20(var%20i%3DcentreX%3B%20i%20%3C%3D%20width%3B%20i%3Di%2B10)%7Bcontext.moveTo(i%2C%200)%3Bcontext.lineTo(i%2C%20height)%3Bif%20((centreX-i)%25100%20%3D%3D%200)%20context.lineTo(i%2C%200)%3B%7Dfor%20(var%20i%3DcentreX%3B%20i%20%3E%3D%20-width%3B%20i%3Di-10)%7Bcontext.moveTo(i%2C%200)%3Bcontext.lineTo(i%2C%20height)%3Bif%20((centreX-i)%25100%20%3D%3D%200)%20context.lineTo(i%2C%200)%3B%7Dfor%20(i%3DcentreY%3B%20i%20%3C%3D%20height%3B%20i%3Di%2B10)%7Bcontext.moveTo(0%2C%20i)%3Bcontext.lineTo(width%2C%20i)%3Bif%20((centreY-i)%25100%20%3D%3D%200)%20context.lineTo(0%2C%20i)%3B%7Dfor%20(i%3DcentreY%3B%20i%20%3E%3D%20-height%3B%20i%3Di-10)%7Bcontext.moveTo(0%2C%20i)%3Bcontext.lineTo(width%2C%20i)%3Bif%20((centreY-i)%25100%20%3D%3D%200)%20context.lineTo(0%2C%20i)%3B%7Dcontext.stroke()%3Bcontext.beginPath()%3Bcontext.moveTo(width%2F2%2C%200)%3Bcontext.lineTo(width%2F2%2C%20height)%3Bcontext.moveTo(0%2C%20height%2F2)%3Bcontext.lineTo(width%2C%20height%2F2)%3Bcontext.stroke()%7D)()
+
 function drawCanvasGrid(){
   var x = document.getElementById("scanv");
   var context = x.getContext("2d");
@@ -62,9 +68,6 @@ function drawOpenStratGrid(canvasId){
   context.lineTo(width, height/2);  //redraw origin lines to make them stand out even more
   context.stroke();
 }
-
-//drawCanvasGrid()
-drawOpenStratGrid();
 
 document.body.onmousemove = function(e){
   if (document.getElementById("mouseFeedback") == null) {
